a44017df302728c89e7cf486e72badfe7fd496d6
baek minu <61592860+dovi0129@users.noreply.github.com>
2025-09-27 18:21:29 +0900

    Update week5_name_2025xxxx.c

--- week5/week5_name_2025xxxx.c
+++ week5/week5_name_2025xxxx.c
@@ -1,38 +1,34 @@
 #include <stdio.h>
 
 int main(void) {
-	int mid, fin; //Middle or Final Score
-	double avg;  //Average Score(Mid 40%, Fin 60%)
-	int avg_int; //Average Score(Integer)
-	char grade; //Grade(A~F)
-	
-	
-	//Input Score as INT
-	printf("Input Middle Score: ");
-	scanf("   ", &mid);
-	
-	printf("Input Final Score: ");
-	scanf("   ", &fin);
-	
-	
-	//Middle 40%, Final 60%
-	avg = mid *    + fin *   ;
-	
-	printf("Average Score: %0.2f\n", avg);
-	
-	//Cast it to an Integer.
-	avg_int = ;
-	
-	printf("Average Score(Integer): %d\n", avg_int);
-	
-	//Decide the final grade.
-	grade = () ? 'A'
-			: () ? 'B'
-			: () ? 'C'
-			: () ? 'D' : 'F';
-			
-	printf("Grade: %c\n", grade);
-	
-	return 0;
+    int mid, fin;          // Middle or Final Score
+    double avg;            // Average Score(Mid 40%, Fin 60%)
+    int avg_int;           // Average Score(Integer)
+    char grade;            // Grade(A~F)
 
+    // Input Score as INT
+    printf("Input Middle Score: ");
+    scanf(/* #0 */, &mid);
+
+    printf("Input Final Score: ");
+    scanf(/* #1 */, &fin);
+
+    // Middle 40%, Final 60%
+    avg = mid * /* #2 */ + fin * /* #3 */;
+
+    printf("Average Score: %0.2f\n", avg);
+
+    // Cast it to an Integer.
+    avg_int = /* #4 */;
+
+    printf("Average Score(Integer): %d\n", avg_int);
+
+    // Decide the final grade.
+    grade = (/* #5 */) ? 'A'
+          : (/* #6 */) ? 'B'
+          : (/* #7 */) ? 'C'
+          : (/* #8 */) ? 'D' : 'F';
+
+    printf("Grade: %c\n", grade);
+    return 0;
 }
