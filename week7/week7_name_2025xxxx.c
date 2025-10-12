d45e937637d5420ecb4a7fe42ad157aef40540e8
baek minu <61592860+dovi0129@users.noreply.github.com>
2025-10-12 17:15:41 +0900

    Update week7_name_2025xxxx.c

--- week7/week7_name_2025xxxx.c
+++ week7/week7_name_2025xxxx.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+int main(void) {
+int sum = 0; // 합을 저장할 변수
+
+for (int i = 1; i <= 30; i++) {
+    sum += (i * i + 1); // i^2 + 1을 누적
+}
+
+printf("Sum = %d\n", sum);
+return 0;
+}
